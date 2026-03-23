0c652b10c8b6902be5af290628851815ecee2a9c
jyhwang0631 <jiyonghwang0631@gmail.com>
2026-03-23 17:19:13 +0900

    clean init

---
+++ CMakeFiles/Atmega128_Project.elf.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Atmega128_Project.elf.
+# This may be replaced when dependencies are built.
---
+++ CMakeFiles/Atmega128_Project.elf.dir/src/led2.c.obj.d
@@ -0,0 +1,9 @@
+CMakeFiles/Atmega128_Project.elf.dir/src/led2.c.obj: \
+ /home/jyhwang0631/jyhwang0631/atmega128/src/led2.c \
+ /usr/lib/avr/include/avr/io.h /usr/lib/avr/include/avr/sfr_defs.h \
+ /usr/lib/avr/include/inttypes.h /usr/lib/gcc/avr/7.3.0/include/stdint.h \
+ /usr/lib/avr/include/stdint.h /usr/lib/avr/include/avr/iom128.h \
+ /usr/lib/avr/include/avr/portpins.h /usr/lib/avr/include/avr/common.h \
+ /usr/lib/avr/include/avr/version.h /usr/lib/avr/include/avr/fuse.h \
+ /usr/lib/avr/include/avr/lock.h /usr/lib/avr/include/util/delay.h \
+ /usr/lib/avr/include/util/delay_basic.h /usr/lib/avr/include/math.h
